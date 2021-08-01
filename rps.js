bf2a02c7eebbea6f8dc2ca94412360e8bea391a2
tiger55md <karrunucu@gmail.com>
2021-08-01 11:49:58 +0100

    Added visualization of plays

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
-const buttons = document.querySelectorAll(".selection");
-console.log(buttons.length);
+const buttons = document.querySelectorAll(".selection"); //Used to choose all of the input elements
+console.log(buttons.length); //just verifying the length, to check if everything works fine
 
 
 
@@ -7,6 +7,7 @@ console.log(buttons.length);
 let playerScore = 0;
 let computerScore = 0;
 
+//this function randomly chooses what the computer is going to play
 function computerPlay(){
     let rpsChoice = [ "Rock", "Paper", "Scissors"];
     let randomIndex = Math.floor(Math.random()*rpsChoice.length);
@@ -14,12 +15,12 @@ function computerPlay(){
 
 }
 
+
+//the main function, evaluates everything that is played
 function playRound(playerSelection){
     let computerSelection = computerPlay();
     let result;
-
-
-
+    changeImage(playerSelection, computerSelection);
     if( (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors") || (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper")
     || (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") ){
         playerScore++;
@@ -42,6 +43,48 @@ function playRound(playerSelection){
     }
 
 
+}
+
+function changeImage(playerSelection, computerSelection){
+    let playerImg = document.getElementsByClassName("player")[0];
+    let computerImg = document.getElementsByClassName("computer")[0];
+    if(playerSelection.toUpperCase() === 'ROCK'){
+        playerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
+        if(computerSelection.toUpperCase() === "ROCK"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
+        }
+        else if(computerSelection.toUpperCase() === "PAPER"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/paper.png";
+        }
+        else{
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/scissors.png";
+        }
+    }
+    else if(playerSelection.toUpperCase() === "PAPER"){
+        playerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/paper.png";
+        if(computerSelection.toUpperCase() === "ROCK"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
+        }
+        else if(computerSelection.toUpperCase() === "PAPER"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/paper.png";
+        }
+        else{
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/scissors.png";
+        }
+    }
+    else{
+        playerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/scissors.png";
+        if(computerSelection.toUpperCase() === "ROCK"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/rock.png";
+        }
+        else if(computerSelection.toUpperCase() === "PAPER"){
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/paper.png";
+        }
+        else{
+            computerImg.src = "https://res.cloudinary.com/itsellej/image/upload/v1533400140/rock-paper-scissors/scissors.png";
+        }
+
+    }
 }
 
 buttons.forEach(button =>{
